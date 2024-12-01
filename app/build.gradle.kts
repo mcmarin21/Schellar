a198369e58eb715d7f6a872d71bc24fbfd9dfe90
Carlos Marin <candresm2200@alumno.ipn.mx>
2024-12-01 00:39:22 -0600

    Navegacion con fragmentos login

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -33,6 +33,8 @@ android {
 
 dependencies {
 
+    implementation("androidx.navigation:navigation-fragment:2.8.4")
+    implementation("androidx.navigation:navigation-ui:2.8.4")
     implementation(libs.appcompat)
     implementation(libs.material)
     implementation(libs.activity)
---
+++ app/src/main/java/com/mcmarin21/schellar/SartupPage.java
@@ -0,0 +1,38 @@
+package com.mcmarin21.schellar;
+
+import android.os.Bundle;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
+import androidx.navigation.Navigation;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Button;
+
+public class SartupPage extends Fragment implements View.OnClickListener {
+
+    @Override
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+        Button login = view.findViewById(R.id.mainBtnLogin);
+        login.setOnClickListener(this);
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_sartup_page, container, false);
+    }
+
+    @Override
+    public void onClick(View v) {
+        int opc = v.getId();
+        if(opc == R.id.mainBtnLogin){
+            Navigation.findNavController(v).navigate(R.id.login_page);
+        }
+    }
+}
