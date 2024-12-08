57b7bc7a883c46143132338e230e46dc2cdb2cbe
Carlos Marin <candresm2200@alumno.ipn.mx>
2024-12-07 18:59:19 -0600

    Cierre de sesión

--- app/src/main/java/com/mcmarin21/schellar/app/Configuracion.java
+++ app/src/main/java/com/mcmarin21/schellar/app/Configuracion.java
@@ -1,21 +1,53 @@
 package com.mcmarin21.schellar.app;
 
+import android.content.Context;
+import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Bundle;
 
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
 
 import com.mcmarin21.schellar.R;
+import com.mcmarin21.schellar.activities.MainActivity;
 
-public class Configuracion extends Fragment {
+public class Configuracion extends Fragment implements View.OnClickListener {
+
+    Button logout;
+
+    @Override
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+
+        logout = view.findViewById(R.id.configuracion_bt_logout);
+        logout.setOnClickListener(this);
+
+    }
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
         return inflater.inflate(R.layout.fragment_configuracion, container, false);
+    }
+
+    @Override
+    public void onClick(View v) {
+        if(v.getId() == logout.getId()){
+            SharedPreferences sharedPreferences = getActivity().getSharedPreferences("session", Context.MODE_PRIVATE);
+            SharedPreferences.Editor editor = sharedPreferences.edit();
+            editor.clear();
+            editor.apply();
+            Intent intent = new Intent(getContext(), MainActivity.class);
+            startActivity(intent);
+            getActivity().finish();
+            return;
+        }
     }
 }
